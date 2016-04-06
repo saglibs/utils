9e1e605eb73fa7917eafba7a41410d86620919af
sagnitude <leyicong@gmail.com>
2016-04-06 13:42:13 +0800

    gulp task code refined..

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,6 @@
-var browserify = require('browserify');
-var es         = require('event-stream');
-var gulp       = require('gulp');
-var buffer     = require('gulp-buffer');
-var sourcemaps = require('gulp-sourcemaps');
-var tap        = require('gulp-tap');
-var uglify     = require('gulp-uglify');
-var gutil      = require('gulp-util');
-var source     = require('vinyl-source-stream');
-var del        = require('del');
+var gulp = require('gulp');
+var gulpTask = require('coreutil/gulptask');
 
 gulp.task('default', function () {
-
-    del(['./dist/**/*']);
-
-    //noinspection JSUnresolvedFunction
-    var coreStream = browserify('./core.js').bundle()
-        .pipe(source('utils.bundle.js'))
-        .pipe(gulp.dest('./dist'));
-
-    //noinspection JSUnresolvedFunction
-    var utilStream = browserify('./utils.js').bundle()
-        .pipe(source('core.bundle.js'))
-        .pipe(gulp.dest('./dist'));
-
-    var minStream = gulp.src(['./utils.js', './core.js'], {read: false}) // no need of reading file because browserify does.
-        // transform file objects using gulp-tap plugin
-        .pipe(tap(function (file) {
-            gutil.log('bundling ' + file.path);
-            // replace file contents with browserify's bundle stream
-            //noinspection JSUnresolvedFunction
-            file.contents = browserify(file.path, {debug: false}).bundle();
-        }))
-        // transform streaming contents into buffer contents (because gulp-sourcemaps does not support streaming contents)
-        .pipe(buffer())
-        // load and init sourcemaps
-        .pipe(sourcemaps.init({loadMaps: true}))
-        .pipe(uglify())
-        // write sourcemaps
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('./dist'));
-
-    return es.concat(coreStream, utilStream, minStream);
+    return gulpTask(['./core.js', './utils.js']);
 });
